9a6dacf95957e5ccf5a93839e209595768b9962a
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-07-05 14:15:36 +0300

    added some cart to cart page

---
+++ client-reactjs/src/components/ProductItemCart.tsx
@@ -0,0 +1,89 @@
+import { ChangeEvent, useState } from "react";
+
+const ProductItemCart = () => {
+
+    const [inputAmountValue, setInputAmountValue] = useState(1);
+
+    const [valueDiscount, setValueDiscount] = useState<number>(0);  // указываем состояние для скидки в процентах,указываем ему в generic тип number,то есть в этом состоянии будут числа,но если указать дефолтное значение состоянию useState,то автоматически ему выдается тип тех данных,которые мы указали по дефолту,в данном случае указали этому состоянию по дефолту значение 0,поэтому тип в generic здесь можно было не указывать,так как он был бы присвоен автоматически как number
+
+
+    // функция для изменения значения инпута количества товара,указываем параметру e(event) тип как ChangeEvent<HTMLInputElement>
+    const changeInputAmountValue = (e: ChangeEvent<HTMLInputElement>) => {
+
+        // если текущее значение инпута > 99,то изменяем состояние инпута цены на 99,указываем + перед e.target.value,чтобы перевести текущее значение инпута из строки в число
+        if (+e.target.value > 99) {
+
+            setInputAmountValue(99);
+
+        } else if (+e.target.value <= 0) {
+
+            // если текущее значение инпута < или равно 0,то ставим значение инпуту 0,чтобы меньше 0 не уменьшалось
+            setInputAmountValue(0);
+
+        } else {
+
+            setInputAmountValue(+e.target.value); // изменяем состояние инпута цены на текущее значение инпута,указываем + перед e.target.value,чтобы перевести текущее значение инпута из строки в число
+
+        }
+
+    }
+
+    const handlerMinusAmountBtn = () => {
+
+        // если значение инпута количества товара больше 1,то изменяем это значение на - 1,в другом случае указываем ему значение 1,чтобы после нуля или 1 не отнимало - 1
+        if (inputAmountValue > 1) {
+
+            setInputAmountValue((prev) => prev - 1);
+
+        } else {
+
+            setInputAmountValue(1);
+
+        }
+
+    }
+
+    const handlerPlusAmountBtn = () => {
+
+        // если значение инпута количества товара меньше 99 и больше или равно 0,то изменяем это значение на + 1,в другом случае указываем ему значение 99,чтобы больше 99 не увеличивалось
+        if (inputAmountValue < 99 && inputAmountValue >= 0) {
+
+            setInputAmountValue((prev) => prev + 1);
+
+        } else {
+
+            setInputAmountValue(99);
+
+        }
+
+    }
+
+    return (
+        <div className="sectionCart__productsBlock-product">
+            <div className="sectionCart__product-detailsBlock">
+                <img src="/images/sectionNewArrivals/Rectangle 25.jpg" alt="" className="sectionCart__product-img" />
+                <div className="sectionCart__product-detailsBlockInfo">
+                    <h1 className="sectionCart__product-name">Blue Flower Print Crop Top</h1>
+                    <p className="sectionCart__product-size">Size: M</p>
+                </div>
+            </div>
+            <p className="sectionCart__product-price">$29.00</p>
+            <div className="sectionProductItemPage__cartBlock-inputBlock">
+                <button className="cartBlock__inputBlock-btn cartBlock__inputBlock-btn--minus" onClick={handlerMinusAmountBtn}>
+                    <img src="/images/sectionProductItemPage/Minus.png" alt="" className="cartBlock__inputBlock-btnImg" />
+                </button>
+                <input type="number" className="cartBlock__inputBlock-input" value={inputAmountValue} onChange={changeInputAmountValue} />
+                <button className="cartBlock__inputBlock-btn cartBlock__inputBlock-btn--plus" onClick={handlerPlusAmountBtn}>
+                    <img src="/images/sectionProductItemPage/Plus.png" alt="" className="cartBlock__inputBlock-btnImg" />
+                </button>
+            </div>
+            <p className="sectionCart__product-price">$29.00</p>
+            <button className="sectionCart__product-deleteBtn">
+                <img src="/images/sectionCart/deletecon.png" alt="" className="sectionCart__product-deleteBtnImg" />
+            </button>
+        </div>
+    )
+
+}
+
+export default ProductItemCart;
--- client-reactjs/src/pages/Cart.tsx
+++ client-reactjs/src/pages/Cart.tsx
@@ -1,9 +1,67 @@
+import { RefObject, useRef } from "react";
+import ProductItemCart from "../components/ProductItemCart";
+import SectionUnderTop from "../components/SectionUnderTop";
+import { useIsOnScreen } from "../hooks/useIsOnScreen";
 
 
 const Cart = () => {
-    return(
+
+    const sectionCatalog = useRef<HTMLElement>(null); // создаем ссылку на html элемент и помещаем ее в переменную sectionTopRef,указываем тип в generic этому useRef как HTMLElement(иначе выдает ошибку),указываем в useRef null,так как используем typeScript
+
+    const onScreen = useIsOnScreen(sectionCatalog as RefObject<HTMLElement>); // вызываем наш хук useIsOnScreen(),куда передаем ссылку на html элемент(в данном случае на sectionTop),указываем тип этой ссылке на html элемент как RefObject<HTMLElement> (иначе выдает ошибку),и этот хук возвращает объект состояний,который мы помещаем в переменную onScreen
+
+    return (
         <main className="main">
-            Cart
+            <SectionUnderTop subtext="Cart" />
+            <section id="sectionCatalog" className={onScreen.sectionCatalogIntersecting ? "sectionCatalog sectionCatalog__active sectionCart" : "sectionCatalog sectionCart"} ref={sectionCatalog}>
+                <div className="container">
+                    <div className="sectionCart__inner">
+                        <div className="sectionCart__table">
+                            <div className="sectionCart__table-names">
+                                <p className="sectionCart__table-name">Product Details</p>
+                                <p className="sectionCart__table-name">Price</p>
+                                <p className="sectionCart__table-name">Quantity</p>
+                                <p className="sectionCart__table-name">Subtotal</p>
+                                <p className="sectionCart__table-name">Action</p>
+                            </div>
+                            <div className="sectionCart__table-mainBlock">
+
+                                <div className="sectionCart__table-productsBlock">
+
+                                    <ProductItemCart />
+
+                                </div>
+
+                                <div className="sectionCart__table-bottomBlock">
+                                    <button className="sectionCart__table-bottomBlockClearBtn">Clear Cart</button>
+
+                                    <button className="sectionCart__table-bottomBlockUpdateBtn">Update Cart</button>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div className="sectionCart__bill">
+                            <div className="sectionCart__bill-topBlock">
+                                <div className="sectionCart__bill-topBlockItem">
+                                    <p className="bill__topBlockItem-text">Subtotal</p>
+                                    <p className="bill__topBlockItem-subtext">$513.00</p>
+                                </div>
+                                <div className="sectionCart__bill-topBlockItem">
+                                    <p className="bill__topBlockItem-text">Shipping</p>
+                                    <p className="bill__topBlockItem-subtext">$5.00</p>
+                                </div>
+                            </div>
+                            <div className="sectionCart__bill-bottomBlock">
+                                <div className="sectionCart__bill-topBlockItem">
+                                    <p className="bill__bottomBlockItem-text">Total</p>
+                                    <p className="bill__bottomBlockItem-subtext">$518.00</p>
+                                </div>
+                                <button className="sectionCart__bill-btn">Proceed To Checkout</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </main>
     )
 }
