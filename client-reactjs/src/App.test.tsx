747e925837b1800dabd74980986c2d01d1db7a97
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-07-28 16:24:33 +0300

    added some for tests and added some comments to app tsx file

--- client-reactjs/src/App.test.tsx
+++ client-reactjs/src/App.test.tsx
@@ -3,8 +3,10 @@ import { render, screen } from '@testing-library/react';
 
 import AboutUs from './pages/AboutUs';
 
+import 'intersection-observer';
+
 test('render text', () => {
   render(<AboutUs />);
-  const linkElement = screen.getByText(/AboutUs/i);
+  const linkElement = screen.getByText(/100% Trusted Fashion Store/i);
   expect(linkElement).toBeInTheDocument();
 });
