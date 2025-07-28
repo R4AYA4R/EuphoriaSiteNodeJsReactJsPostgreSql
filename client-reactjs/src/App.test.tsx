7e519b1ab69aba00f12acfb7124e8b171a377487
R4AYA4R <shilovich.yaroslav@gmail.com>
2025-07-28 16:26:36 +0300

    added some comments to app test tsx file

--- client-reactjs/src/App.test.tsx
+++ client-reactjs/src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react';
 
 import AboutUs from './pages/AboutUs';
 
-import 'intersection-observer';
+import 'intersection-observer'; // импортируем вручную intersection-observer(который до этого установили для dev режима(режима разработки) командой npm i intersection-observer --save-dev),чтобы не было ошибки,что не может найти intersection observer при запуске тестов
 
 test('render text', () => {
   render(<AboutUs />);
